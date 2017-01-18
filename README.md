657076cb2f549a4bfbe87e08d27140713202a239
xjouyi <xjouyi@163.com>
2017-01-18 10:41:45 +0800

    1.0

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # SinaWeibo
 python脚本,支持非官方API 方式登录新浪微博发送消息和图片
+
+## 配置文件config.json
+
+>WeiboLogin : 微博账号
+
+>WeiboPassWord ：微博密码
+
+>WeiboUserName : 微博中文名
+
+
---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from ConfigUtils import Config
+from Weibo import Weibo
+
+if __name__ == '__main__':
+    config = Config()
+    weibo = Weibo(config)
+	
+
+    
+    
+	
